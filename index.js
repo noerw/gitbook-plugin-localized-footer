b64890254d2a43ad7e89e997b7586cd3c5d0367c
noerw <noerw@gmx.de>
2017-05-11 22:12:32 +0200

    add .localized-footer, deprecate #page-footer

--- index.js
+++ index.js
@@ -11,16 +11,21 @@ module.exports = {
 
             try {
                 fs.statSync(this.resolve(cfg.filename));
+                hasFooterFile = true;
             } catch (e) {
                 hasFooterFile = false;
                 return;
             }
 
-            hasFooterFile = true;
+            deprecationWarning(this);
 
             this.readFileAsString(cfg.filename)
-                .then(function (data) { return _this.renderBlock('markdown',data); }, this.log.error)
-                .then(function(html) { footerString = html; }, this.log.error);
+                .then(function(data) {
+                    return _this.renderBlock('markdown', data);
+                }, this.log.error)
+                .then(function(html) {
+                    footerString = html;
+                }, this.log.error);
         },
         'page:before': function(page) {
             // append to the website renderer only
@@ -33,8 +38,31 @@ module.exports = {
         'localizedfooter': {
             process: function(block) {
                 var hline = cfg.hline ? '<hr>' : '';
-                return '<div class="page-footer">' + hline + block.body + '</div>';
+                return '<div id="page-footer" class="localized-footer">' + hline + block.body + '</div>';
             }
         }
     }
 };
+
+function deprecationWarning(ctx) {
+
+    if (!hasFooterFile) return;
+
+    // search website.css for deprecated style selector
+    ctx.readFileAsString(ctx.config.get('styles.website'))
+        .then(function(css) {
+            var lines = css.split('\n');
+
+            for (var i = 0; i < lines.length; i++) {
+                if (lines[i].search('#page-footer') !== -1) {
+                    return ctx.log.warn(
+                        '[localized-footer] the css selector \'#page-footer\'' +
+                        'is deprecated, use \'.localized-footer\' instead.'
+                    )
+                }
+            };
+        })
+        .catch(function(err) {
+            // no style file present, ignore
+        });
+}
